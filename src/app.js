b21b946c51025b1993a5b5ee87c9dfb6ee8803da
dannyelgjl <dannyelgjl@gmail.com>
2020-04-08 20:38:37 -0300

    configurando conexão com o paypal

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from "./routes";
+import paypal from "paypal-rest-sdk";
 
 class App {
   constructor() {
@@ -11,6 +12,15 @@ class App {
 
   middleware() {
     this.server.use(express.json());
+    this.server.use(
+      paypal.configure({
+        mode: "sandbox", //sandbox or live
+        client_id:
+          "AanXona2ffYXMiZwLttM-M-f1-Mbpr7wpnNRsGTP4baxKMJYzn3TrxV2lCnkuaGX8S7kFStRAfF-pqf-",
+        client_secret:
+          "EF26nDBGKyDFsWvrYqQh7jv9McaJBsufHvc3mIPIYjH_bV814cuvY1va21ePrkNgbyqZjtns5Qr2k4eQ",
+      })
+    );
   }
 
   routes() {
